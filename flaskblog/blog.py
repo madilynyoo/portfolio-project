23e292285924e01c7881cea680fa24df3667393d
madilynyoo <madilyn@kavsports.com>
2025-02-03 21:25:41 -0800

    back to debugging

--- flaskblog/blog.py
+++ flaskblog/blog.py
@@ -11,6 +11,7 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
+    print(db)
     blob = db.execute(
         'SELECT p.id, title, body, date_posted, author_id, username'
         ' FROM posts p JOIN user u ON p.author_id = u.id'
