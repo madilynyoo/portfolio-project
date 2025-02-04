8fb5532e8359c091b69eb9df5fcaf916091772bc
madilynyoo <madilyn@kavsports.com>
2025-02-03 21:33:16 -0800

    changed variable names back

--- flaskblog/blog.py
+++ flaskblog/blog.py
@@ -12,12 +12,12 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     print(db)
-    blob = db.execute(
+    posts = db.execute(
         'SELECT p.id, title, body, date_posted, author_id, username'
         ' FROM posts p JOIN user u ON p.author_id = u.id'
         ' ORDER BY date_posted DESC'
     ).fetchall()
-    return render_template('index.html', posts=blob)    
+    return render_template('index.html', posts=posts)    
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
